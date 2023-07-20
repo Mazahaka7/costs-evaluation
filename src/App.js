22f977ebe783bae3ff09350d1295101b130a2c8c
git <dima.it.ua@gmail.com>
2023-07-20 22:30:19 +0300

    1st part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Costs from "./components/Costs";
 
 function App() {
+  const costs = [
+    {
+      date: new Date(2023, 6, 20),
+      description: "Курси React Pro",
+      amount: 11.33,
+    },
+    {
+      date: new Date(2023, 5, 20),
+      description: "Курточка Ricardo",
+      amount: 49.99,
+    },
+    {
+      date: new Date(2023, 4, 20),
+      description: "Кросівки TommyJeans",
+      amount: 90.99,
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Start react learning</h1>
+      <Costs costs={costs} />
     </div>
   );
 }
